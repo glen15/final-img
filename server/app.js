dc50748bdf322b46276f6b2ef96d2be591959924
glen <glen910115@gmail.com>
2021-10-13 08:22:07 +0900

    test

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('tiny'));
 
 app.use((req, res, next) => {
   res.send('hi');
 });
 
-app.listen(8080);
+app.listen(80);
